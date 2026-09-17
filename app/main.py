3f90e9480b15cc2fc2c435dfeffabc94b3ee1bff
i-official <io.yarbess@gmail.com>
2026-09-17 14:04:53 +0300

    fixed whitespace and lines

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ def format_linter_error(errors: dict) -> dict:
         "source": "flake8"
     }
 
+
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [format_linter_error(el) for el in errors],
@@ -14,4 +15,5 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
         "status": "passed" if not errors else "failed"}
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(path,errors) for path, errors in linter_report.items()]
+    return [format_single_linter_file(path, errors) for path,
+    errors in linter_report.items()]
